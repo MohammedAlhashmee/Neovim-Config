0f3af75bce0b4db5bc166f56c10a81d2510eec38
MohammedAlhashmee <tookitseriousaboutlife@gmail.com>
2026-04-10 16:02:05 +0200

    treesitter fixed

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,8 +1,17 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-  config = function()
-      require('nvim-treesitter').install { 'cpp', 'python', 'lua' }
-  end
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    callback = function()
+        require('nvim-treesitter').install { 'cpp', 'python', 'lua' }
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'svelte', 'python', 'typescript', 'astro' },
+            callback = function()
+                vim.treesitter.start()                                    -- highlighting
+                vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'     -- folds
+                vim.wo.foldmethod = 'expr'
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+            end,
+        })
+    end
 }
