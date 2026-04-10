178ad7d247ff935db8eb6aa8ea3a88271eeffa25
MohammedAlhashmee <tookitseriousaboutlife@gmail.com>
2026-04-10 14:40:08 +0200

    mason lsp config added

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -1,18 +1,9 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        servers = { "clangd", "ts_ls", "lua_ls", "pyright", "vimls" }
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
-        for _, server in ipairs(servers) do
-            vim.lsp.config(server, {
-                capabilities = capabilities,
-            })
-        end
-        vim.lsp.enable(servers)
-    end,
+
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
     dependencies = {
-        "mason-org/mason.nvim",
-        opts = {
+        { "mason-org/mason.nvim", opts = {
             ui = {
                 icons = {
                     package_installed = "✓",
@@ -20,6 +11,8 @@ return {
                     package_uninstalled = "✗"
                 }
             }
-        }
-    }
+        } },
+        "neovim/nvim-lspconfig",
+    },
 }
+
